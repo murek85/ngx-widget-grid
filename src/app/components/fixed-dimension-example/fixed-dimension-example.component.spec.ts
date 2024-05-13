ec4e568a28b28c409172a2dbce49b4afd922044f
Marcin Murawski <marcinmur@gmail.com>
2024-05-13 12:15:42 +0200

    [lib] ngx-widget-grid

---
+++ src/app/components/fixed-dimension-example/fixed-dimension-example.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
+
+import { FixedDimensionExampleComponent } from "./fixed-dimension-example.component";
+
+describe("FixedDimensionExampleComponent", () => {
+  let component: FixedDimensionExampleComponent;
+  let fixture: ComponentFixture<FixedDimensionExampleComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [FixedDimensionExampleComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FixedDimensionExampleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+});
